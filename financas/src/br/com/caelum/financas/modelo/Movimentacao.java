a2a16a9a982995a8ce174088b170099be6d12735
gabrieldarlan <gabriel.darlan88@gmail.com>
2020-03-12 08:21:23 -0300

    AULA 05 - 02 MUITOS PARA MUITOS E UM PARA UM

--- financas/src/br/com/caelum/financas/modelo/Movimentacao.java
+++ financas/src/br/com/caelum/financas/modelo/Movimentacao.java
@@ -2,6 +2,7 @@ package br.com.caelum.financas.modelo;
 
 import java.math.BigDecimal;
 import java.util.Calendar;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -9,7 +10,9 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 
@@ -32,6 +35,9 @@ public class Movimentacao {
 
 	@ManyToOne
 	private Conta conta;
+
+	@ManyToMany
+	private List<Categoria> categorias;
 
 	public Integer getId() {
 		return id;
@@ -79,6 +85,14 @@ public class Movimentacao {
 
 	public void setDescricao(String descricao) {
 		this.descricao = descricao;
+	}
+
+	public List<Categoria> getCategorias() {
+		return categorias;
+	}
+
+	public void setCategorias(List<Categoria> categorias) {
+		this.categorias = categorias;
 	}
 
 }
